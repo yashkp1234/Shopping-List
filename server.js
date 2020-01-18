77ecf8aa75a3b4a4f3df58e0e525bdd14d43fcd3
yashkp1234 <yashkp123@gmail.com>
2020-01-18 17:48:38 -0500

    backend complete

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-test
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+
+// Use routes
+app.use('/api/items', items);
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started at port ${port}`));
+
